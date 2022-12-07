17e4fc58669a6f8d557a319e996d06b45ff41ec8
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-12-07 21:00:41 +0100

    function to change saturation value

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const box = document.querySelector(".box");
 const speedBtns = document.querySelectorAll('[data-settings="speed"]');
 const colorBtns = document.querySelectorAll('[data-settings="color"]');
 const slider = document.querySelector("#slider");
-const sliderInfo = document.querySelector("slider-info");
+const sliderInfo = document.querySelector(".slider-info");
 
 const squares = 546;
 let sliderValue = 70;
@@ -50,7 +50,12 @@ function handleColorRange() {
   range = parseInt(this.dataset.colorRange);
 }
 
+const showSliderInfo = () => {
+  sliderInfo.textContent = slider.value;
+};
+
 speedBtns.forEach((btn) => btn.addEventListener("click", handleSpeed));
 colorBtns.forEach((btn) => btn.addEventListener("click", handleColorRange));
+slider.addEventListener("mousemove", showSliderInfo);
 
 createSquare();
